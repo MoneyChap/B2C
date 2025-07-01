3489e980a36ed2df8cc3d47ad3cad0f86ac70cd2
MoneyChap <eriks.osis@whykingz.com>
2025-07-01 20:33:03 +0300

    adding imgs

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -7,8 +7,10 @@ import SportsBalls from "../../assets/SportsBalls.png";
 import orangeBG from "../../assets/orangeBG.png";
 import blueBG from "../../assets/bluefadebg.png";
 import greyBG from "../../assets/greyBG.png";
+import purpleBG from "../../assets/purpleBG.jpeg";
 import liveClock from "../../assets/liveClock.png";
 import brain from "../../assets/brain.png";
+import Trophy from "../../assets/80Trophy.png";
 
 
 const HomePage = () => {
@@ -74,9 +76,9 @@ const HomePage = () => {
                         <div className={styles.scrollItem}>
                             <div className={styles.bluefade}>
                                 <div className={styles.itemImgContainer}>
-                                    <img className={styles.itemImg} src={lightningImg} alt={lightningImg} />
+                                    <img className={styles.itemImg} src={Trophy} alt={Trophy} />
                                 </div>
-                                <p className={styles.itemText}>Experience sports betting like never before</p>
+                                <p className={styles.itemText}>Up to 80% accuracy over 5 years</p>
                             </div>
                         </div>
                         <div className={styles.scrollItem}>
@@ -95,7 +97,7 @@ const HomePage = () => {
                 </div>
             </section>
             <section className={styles.sportAmountSection}>
-                <div className={styles.sportAmountContainer}  style={{backgroundImage: `url(${blueBG})`}}>
+                <div className={styles.sportAmountContainer} style={{ backgroundImage: `url(${blueBG})` }}>
                     <div className={styles.sportAmountMainContainer}>
                         <div className={styles.sportAmountTextContainer}>
                             <p>SPORTSBETTING, BUT <strong>EASY</strong></p>
@@ -163,9 +165,9 @@ const HomePage = () => {
                 </div>
             </section>
             <section className={styles.sportAmountSection}>
-                <div className={styles.sportAmountContainer}  style={{backgroundImage: `url(${orangeBG})`, backgroundPosition: "bottom"}}>
-                    <div className={styles.sportAmountMainContainer} style={{flexDirection: "row-reverse"}}>
-                        <div className={styles.sportAmountTextContainer} style={{alignItems: "end", textAlign: "right"}}>
+                <div className={styles.sportAmountContainer} style={{ backgroundImage: `url(${orangeBG})`, backgroundPosition: "bottom" }}>
+                    <div className={styles.sportAmountMainContainer} style={{ flexDirection: "row-reverse" }}>
+                        <div className={styles.sportAmountTextContainer} style={{ alignItems: "end", textAlign: "right" }}>
                             <p>REAL TIME <strong>UPDATES</strong></p>
                             <h1>Get updates for live matches</h1>
                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum cupiditate atque sunt quidem, veniam nihil reiciendis libero cumque. Aut animi itaque omnis ducimus dolorem labore necessitatibus rerum suscipit fugiat magni!</p>
@@ -231,7 +233,7 @@ const HomePage = () => {
                 </div>
             </section>
             <section className={styles.sportAmountSection}>
-                <div className={styles.sportAmountContainer}   style={{backgroundImage: `url(${greyBG})`}}>
+                <div className={styles.sportAmountContainer} style={{ backgroundImage: `url(${greyBG})` }}>
                     <div className={styles.sportAmountMainContainer}>
                         <div className={styles.sportAmountTextContainer}>
                             <p>SPORTSBETTING, BUT <strong>EASY</strong></p>
@@ -289,6 +291,74 @@ const HomePage = () => {
                         </div>
                         <div className={styles.sportAmountImgContainer}>
                             <img src={brain} alt={brain} />
+                        </div>
+                    </div>
+                    <div className={styles.sportsCarousel}>
+                        <div className={styles.sportsCarouselItem}>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.sportAmountSection}>
+                <div className={styles.sportAmountContainer} style={{ backgroundImage: `url(${purpleBG})`, backgroundPosition: "bottom" }}>
+                    <div className={styles.sportAmountMainContainer} style={{ flexDirection: "row-reverse" }}>
+                        <div className={styles.sportAmountTextContainer} style={{ alignItems: "end", textAlign: "right" }}>
+                            <p>REAL TIME <strong>UPDATES</strong></p>
+                            <h1>Get updates for live matches</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum cupiditate atque sunt quidem, veniam nihil reiciendis libero cumque. Aut animi itaque omnis ducimus dolorem labore necessitatibus rerum suscipit fugiat magni!</p>
+                            <Link to="/blank">
+                                <button className={styles.sportAmountBtn}>
+                                    <div className={styles.sportAmountIconContainer}>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                d="M 8 12 L 16 12"
+                                                fill="transparent"
+                                                strokeWidth="2"
+                                                stroke="rgb(0,0,0)"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                            <path
+                                                d="M 12 16 L 16 12 L 12 8"
+                                                fill="transparent"
+                                                strokeWidth="2"
+                                                stroke="rgb(0,0,0)"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path
+                                                d="M 8 12 L 16 12"
+                                                fill="transparent"
+                                                strokeWidth="2"
+                                                stroke="rgb(0,0,0)"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                            <path
+                                                d="M 12 16 L 16 12 L 12 8"
+                                                fill="transparent"
+                                                strokeWidth="2"
+                                                stroke="rgb(0,0,0)"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            />
+                                        </svg>
+                                    </div>
+                                    See more
+                                </button>
+                            </Link>
+                        </div>
+                        <div className={styles.sportAmountImgContainer}>
+                            <img src={Trophy} alt={Trophy} />
                         </div>
                     </div>
                     <div className={styles.sportsCarousel}>
